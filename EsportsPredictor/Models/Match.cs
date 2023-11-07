02d7ce74f5e9ebed27d5b5bd08cc7e2df1d76f7b
SGrinstead <seth.grinstead3299@gmail.com>
2023-11-07 12:22:45 -0800

    Initial Models

---
+++ EsportsPredictor/Models/Match.cs
@@ -0,0 +1,15 @@
+﻿namespace EsportsPredictor.Models
+{
+    public class Match
+    {
+        public int Id { get; set; }
+        public string Slug { get; set; }
+        public string Name { get; set; }
+        public DateTime BeginAt { get; set; }
+        public DateTime EndAt { get; set; }
+        public string MatchType { get; set; }
+        public string Status { get; set; }
+        public string WinnerType { get; set; }
+        public int WinnerId { get; set; }
+    }
+}
---
+++ EsportsPredictor/Models/Videogame.cs
@@ -0,0 +1,9 @@
+﻿namespace EsportsPredictor.Models
+{
+    public class Videogame
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+    }
+}
