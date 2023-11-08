b16fc62ff7a85676654d51e9efb2b46b8e9e1171
SGrinstead <seth.grinstead3299@gmail.com>
2023-11-08 13:01:33 -0800

    matches controller

---
+++ EsportsPredictor/Controllers/MatchesController.cs
@@ -0,0 +1,24 @@
+﻿using EsportsPredictor.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using EsportsPredictor.Models;
+
+namespace EsportsPredictor.Controllers
+{
+	public class MatchesController : Controller
+	{
+		private readonly IPandascoreApiService _pandascoreApiService;
+
+        public MatchesController(IPandascoreApiService pandascoreApiService)
+        {
+			_pandascoreApiService = pandascoreApiService;
+        }
+
+        [Route("/tournaments/{tournamentSlug}/matches")]
+		public async Task<IActionResult> Index(string tournamentSlug)
+		{
+			List<Match> matches = await _pandascoreApiService.GetMatchesAsync(tournamentSlug);
+
+			return View(matches);
+		}
+	}
+}
