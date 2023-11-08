600b05015736d255aaab4bb94f4ec4bf810895ae
SGrinstead <seth.grinstead3299@gmail.com>
2023-11-08 13:59:24 -0800

    datetime fix for real

--- EsportsPredictor/Models/Match.cs
+++ EsportsPredictor/Models/Match.cs
@@ -5,11 +5,33 @@
         public int Id { get; set; }
         public string Slug { get; set; }
         public string Name { get; set; }
-        public DateTime BeginAt { get; set; }
-        public DateTime EndAt { get; set; }
+        public DateTime? Begin_at { get; set; }
+        public DateTime? End_at { get; set; }
         public string MatchType { get; set; }
         public string Status { get; set; }
         public string WinnerType { get; set; }
         public int WinnerId { get; set; }
-    }
+
+		public string GetLocalBeginAtString()
+		{
+            if (Begin_at.HasValue)
+            {
+                DateTime local = (DateTime)Begin_at;
+                local = local.ToLocalTime();
+                return local.ToShortDateString() + " " + local.ToShortTimeString();
+            }
+            else return "Unknown";
+		}
+
+		public string GetLocalEndAtString()
+		{
+			if (End_at.HasValue)
+			{
+				DateTime local = (DateTime)End_at;
+				local = local.ToLocalTime();
+				return local.ToShortDateString() + " " + local.ToShortTimeString();
+			}
+			else return "Unknown";
+		}
+	}
 }
--- EsportsPredictor/Models/Tournament.cs
+++ EsportsPredictor/Models/Tournament.cs
@@ -6,8 +6,8 @@
         public string Slug { get; set; }
         public League League { get; set; }
         public string Name { get; set; }
-        public DateTime Begin_at { get; set; } = new DateTime();
-        public DateTime End_at { get; set; } = new DateTime();
+        public DateTime? Begin_at { get; set; }
+        public DateTime? End_at { get; set; }
         public string Tier { get; set; }
         public string Prizepool { get; set; }
         public Videogame Videogame { get; set; }
@@ -16,16 +16,26 @@
         public List<Team> Teams { get; set; } = new List<Team>();
         public List<Match> Matches { get; set; } = new List<Match>();
 
-        public string GetLocalBeginAtString()
-        {
-            DateTime local = Begin_at.ToLocalTime();
-            return local.ToShortDateString() + " " + local.ToShortTimeString();
-        }
+		public string GetLocalBeginAtString()
+		{
+			if (Begin_at.HasValue)
+			{
+				DateTime local = (DateTime)Begin_at;
+				local = local.ToLocalTime();
+				return local.ToShortDateString() + " " + local.ToShortTimeString();
+			}
+			else return "Unknown";
+		}
 
 		public string GetLocalEndAtString()
 		{
-			DateTime local = End_at.ToLocalTime();
-			return local.ToShortDateString() + " " + local.ToShortTimeString();
+			if (End_at.HasValue)
+			{
+				DateTime local = (DateTime)End_at;
+				local = local.ToLocalTime();
+				return local.ToShortDateString() + " " + local.ToShortTimeString();
+			}
+			else return "Unknown";
 		}
 	}
 }
