2a8f4b8ddc2851ebddfc6f91b9eaaa5f427a84f7
SGrinstead <seth.grinstead3299@gmail.com>
2023-11-08 13:44:08 -0800

    Tournament datetime fix

--- EsportsPredictor/Models/Tournament.cs
+++ EsportsPredictor/Models/Tournament.cs
@@ -6,8 +6,8 @@
         public string Slug { get; set; }
         public League League { get; set; }
         public string Name { get; set; }
-        public DateTime BeginAt { get; set; } = new DateTime();
-        public DateTime EndAt { get; set; } = new DateTime();
+        public DateTime Begin_at { get; set; } = new DateTime();
+        public DateTime End_at { get; set; } = new DateTime();
         public string Tier { get; set; }
         public string Prizepool { get; set; }
         public Videogame Videogame { get; set; }
@@ -15,5 +15,17 @@
         public int? WinnerId { get; set; }
         public List<Team> Teams { get; set; } = new List<Team>();
         public List<Match> Matches { get; set; } = new List<Match>();
-    }
+
+        public string GetLocalBeginAtString()
+        {
+            DateTime local = Begin_at.ToLocalTime();
+            return local.ToShortDateString() + " " + local.ToShortTimeString();
+        }
+
+		public string GetLocalEndAtString()
+		{
+			DateTime local = End_at.ToLocalTime();
+			return local.ToShortDateString() + " " + local.ToShortTimeString();
+		}
+	}
 }
