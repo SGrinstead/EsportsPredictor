018dc54f61286cf57f78e26db7a6a845e8c88bc6
SGrinstead <seth.grinstead3299@gmail.com>
2023-11-13 13:00:43 -0800

    Game filter for tournaments index

--- EsportsPredictor/Controllers/TournamentsController.cs
+++ EsportsPredictor/Controllers/TournamentsController.cs
@@ -13,15 +13,21 @@ namespace EsportsPredictor.Controllers
             _pandascoreApiService = pandascoreApiService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<Videogame> games = await _pandascoreApiService.GetVideogamesAsync();
+
+            return View(games);
         }
 
         [Route("/tournaments/upcoming")]
-        public async Task<IActionResult> Upcoming()
+        public async Task<IActionResult> Upcoming(string? game)
         {
             List<Tournament> tournaments = await _pandascoreApiService.GetUpcomingTournamentsAsync();
+            if(game is not null)
+            {
+                tournaments = tournaments.Where(t => t.Videogame.Slug == game).ToList();
+            }
 
             return View(tournaments);
         }
