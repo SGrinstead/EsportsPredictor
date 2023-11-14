209a3c72499c5f8fc1d8290065b4204b97cbc64f
SGrinstead <seth.grinstead3299@gmail.com>
2023-11-14 13:24:20 -0800

    winner model fix

--- EsportsPredictor/Models/Winner.cs
+++ EsportsPredictor/Models/Winner.cs
@@ -3,6 +3,7 @@
 	public class Winner
 	{
         public int Id { get; set; }
+        public string Name { get; set; }
         public string Type { get; set; }
         public string DetailsPageUrl { get; set; }
     }
