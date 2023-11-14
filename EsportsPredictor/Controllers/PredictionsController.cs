13786955c2be411d4495a60c607a0132a08ba152
SGrinstead <seth.grinstead3299@gmail.com>
2023-11-14 12:08:33 -0800

    check predictions method

--- EsportsPredictor/Controllers/PredictionsController.cs
+++ EsportsPredictor/Controllers/PredictionsController.cs
@@ -1,5 +1,6 @@
 ﻿using EsportsPredictor.DataAccess;
 using EsportsPredictor.Interfaces;
+using EsportsPredictor.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EsportsPredictor.Controllers
@@ -17,7 +18,31 @@ namespace EsportsPredictor.Controllers
 
         public IActionResult Index()
 		{
-			return View();
+			var predictions = _context.Predictions.ToList();
+			CheckPredictions(predictions);
+
+			return View(predictions);
+		}
+
+		private async void CheckPredictions(List<Prediction> predictions)
+		{
+			foreach(var prediction in predictions)
+			{
+				if (!prediction.IsCompleted)
+				{
+					if(DateTime.UtcNow > prediction.Match.Begin_at)
+					{
+						prediction.Match = await _pandascoreApiService.GetMatchAsync(prediction.Match.Slug);
+
+						if(prediction.Match.Status == "finished")
+						{
+							prediction.IsCompleted = true;
+						}
+					}
+					_context.Predictions.Update(prediction);
+				}
+			}
+			_context.SaveChanges();
 		}
 	}
 }
