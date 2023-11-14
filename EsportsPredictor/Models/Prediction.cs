05d973fc25a401f70d8e61171f49ddd4816718c9
SGrinstead <seth.grinstead3299@gmail.com>
2023-11-14 09:40:50 -0800

    basic prediction model

---
+++ EsportsPredictor/Models/Prediction.cs
@@ -0,0 +1,10 @@
+﻿namespace EsportsPredictor.Models
+{
+	public class Prediction
+	{
+        public int Id { get; set; }
+        public Match Match { get; set; }
+        public bool IsCompleted { get; set; }
+        public int PredictedWinnerId { get; set; }
+    }
+}
